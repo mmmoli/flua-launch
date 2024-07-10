93212fc6375500386b6bf14b428cc84b21cc4d8b
Michele Memoli <michele.memoli@gmail.com>
2024-07-10 21:37:03 +0100

    chore(shared): attempts to reduce bundle size

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,46 @@
+import bundleAnalyser from '@next/bundle-analyzer';
 import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      })
+    );
+
+    // return the modified config
+    return config;
+  },
+};
+
+const withBundleAnalyzer = bundleAnalyser({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 // https://github.com/getsentry/sentry-webpack-plugin#options
-export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
+export default withSentryConfig(
+  withBundleAnalyzer(nextConfig, {
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
 
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: '/monitoring',
-  hideSourceMaps: true,
-  disableLogger: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-});
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    tunnelRoute: '/monitoring',
+    hideSourceMaps: true,
+    disableLogger: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+    },
+  })
+);
--- src/lib/pages/room/ui/room-page.tsx
+++ src/lib/pages/room/ui/room-page.tsx
@@ -1,7 +1,9 @@
-import { CallEntity } from '@entities/call';
 import { RoomEntity, RoomModelSlug } from '@entities/room';
 import { WaitingAreaWidget } from '@widgets/rooms/waiting-area';
-import { FC } from 'react';
+import dynamic from 'next/dynamic';
+import { FC, Suspense } from 'react';
+
+const CallEntity = dynamic(() => import('@entities/call').then((mod) => mod.CallEntity));
 
 export interface RoomPageProps {
   params: { slug: RoomModelSlug };
@@ -11,9 +13,11 @@ export const RoomPage: FC<RoomPageProps> = async ({ params }) => {
   return (
     <RoomEntity slug={params.slug}>
       {(room) => (
-        <CallEntity>
-          <WaitingAreaWidget room={room} />
-        </CallEntity>
+        <Suspense>
+          <CallEntity>
+            <WaitingAreaWidget room={room} />
+          </CallEntity>
+        </Suspense>
       )}
     </RoomEntity>
   );
